36688901ef8ecd2ccfb78061ebd2f8761467001c
Nisarg Shah <nisarg0606@gmail.com>
2024-04-18 12:17:14 -0400

    changed type of time slot to array

--- models/venueBooking.js
+++ models/venueBooking.js
@@ -1,24 +1,24 @@
 const moongose = require("mongoose");
 
 const venueBookingSchema = new moongose.Schema({
-    venue: {
-        type: moongose.Schema.Types.ObjectId,
-        ref: 'Venue',
-        required: true,
-    },
-    user: {
-        type: moongose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    booking_date: {
-        type: Date,
-        required: true,
-    },
-    booking_time_slot: {
-        type: String,
-        required: true,
-    },
+  venue: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: "Venue",
+    required: true,
+  },
+  user: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  booking_date: {
+    type: Date,
+    required: true,
+  },
+  booking_time_slot: {
+    type: array,
+    required: true,
+  },
 });
 
 module.exports = moongose.model("VenueBooking", venueBookingSchema);
